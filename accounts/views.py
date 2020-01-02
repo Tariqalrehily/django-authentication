731442e12f4d4ba203a7de8055d50e09b759cdda
Tariqalrehily <Tariqalrehily@gmail.com>
2020-01-02 19:37:45 +0000

    Added the logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
 
 
 #  views here.
 def index(request):
     """Return the index.html file"""
     return render(request, 'index.html')
-    
+
+
+def logout(request):
+    """Log the user out"""
+    auth.logout(request)
+    messages.success(request, "You have sucessfully logged out! Hope to see back soon")
+    return redirect(reverse('index'))
