9e288af2dac6a7d72f79f55d01f6262f62e6ea5b
Tariqalrehily <Tariqalrehily@gmail.com>
2020-01-02 21:24:30 +0000

    Added authentication whether or not user logged in

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
@@ -8,7 +9,7 @@ def index(request):
     """Return the index.html file"""
     return render(request, 'index.html')
 
-
+@login_required
 def logout(request):
     """Log the user out"""
     auth.logout(request)
@@ -18,6 +19,8 @@ def logout(request):
 
 def login(request):
     """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
@@ -29,6 +32,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "welocme back, you have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Sorry, your username or password is incorrect")
     else:
